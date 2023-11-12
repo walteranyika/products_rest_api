c22840756d764c96a5859d4d55e947e9f0af6d3b
walter <walter@tradesy.com>
2023-11-12 09:35:47 +0300

    Handle arbitrary field like email which is not part of the email

--- products/product_urls.py
+++ products/product_urls.py
@@ -4,11 +4,11 @@ from products import views
 
 urlpatterns = [
     # path('<int:pk>', views.ProductDetailApiView.as_view()),
-    path('', views.product_list_create_view),
-    path('<int:pk>/update', views.product_update_view),
+    path('', views.product_list_create_view, name="product-list"),
+    path('<int:pk>/update', views.product_update_view, name="product-edit"),
     path('<int:pk>/delete', views.product_destroy_view),
-    path('<int:pk>', views.product_detail_view),
-    path('mixins', views.product_mixin_view),
-    path('mixins/<int:pk>', views.product_mixin_view),
+    path('<int:pk>', views.product_detail_view, name="product-detail"),
+    # path('mixins', views.product_mixin_view),
+    # path('mixins/<int:pk>', views.product_mixin_view),
 
 ]
--- products/serializers.py
+++ products/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 from products.models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(view_name="product-detail", lookup_field="pk")
+    # email = serializers.EmailField(write_only=True)
 
     class Meta:
         model = Product
@@ -13,8 +17,26 @@ class ProductSerializer(serializers.ModelSerializer):
             "description",
             "price",
             "sale_price",
-            "my_discount"
+            "my_discount",
+            "url",
+            "edit_url"
         ]
+
+    # def create(self, validated_data):
+    #     email = validated_data.pop("email")
+    #     # Can do something with this email here
+    #     obj = super().create(validated_data)
+    #     return obj
+    #
+    # def update(self, instance, validated_data):
+    #     email = validated_data.pop("email")
+    #     return instance
+
+    def get_edit_url(self, obj):
+        request = self.context.get('request')
+        if request is None:
+            return None
+        return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)
 
     def get_my_discount(self, obj):
         if not hasattr(obj, 'id'):
